0a7700f668e176cdb59e17aec47ca3244a15de5d
Arun <arunkn28@gmail.com>
2018-05-18 07:20:38 +0530

    Prod admin stage1

--- GameItems/products/models.py
+++ GameItems/products/models.py
@@ -6,3 +6,8 @@ class Product(models.Model):
     title           = models.CharField(max_length=120)
     description     = models.TextField()
     
+    
+    
+    def __str__(self):
+        return self.title
+    
