96fb28a7dc984fc4c71e647adb347d7dfe7c8053
arun.nambiar <arun.nambiar@global-analytics.com>
2018-05-18 13:21:34 +0530

    Category Schema

--- GameItems/products/models.py
+++ GameItems/products/models.py
@@ -2,12 +2,30 @@ from django.db import models
 
 # Create your models here.
 
+CATEGORYNAMES = ((0,'PS4'),
+                 (1,'PC'))
+
+class Category(models.Model):
+    """Category Table Schema"""
+    category_name           = models.CharField(max_length=50,choices=CATEGORYNAMES)
+    category_description    = models.TextField(max_length=100)
+    category_image          = models.ImageField()
+    created_by              = models.CharField(max_length=50)
+    created_datetime        = models.DateTimeField()
+    modified_by             = models.CharField(max_length=50)
+    modified_datetime       = models.DateTimeField()
+    
+    def __Str__(self):
+        return self.category_name
+
+    
 class Product(models.Model):
     """Product Table Schema"""
     product_sku             = models.CharField(max_length=50)
     product_title           = models.CharField(max_length=120)
     product_description     = models.TextField()
-    category_id             = models.CharField(max_length=50)
+    product_image           = models.ImageField()
+    category_id             = models.ForeignKey(Category)
     quntity_per_unit        = models.IntegerField()
     product_price           = models.DecimalField()
     product_weight          = models.DecimalField()
@@ -18,5 +36,4 @@ class Product(models.Model):
     modified_datetime       = models.DateTimeField()
     
     def __str__(self):
-        return self.title
-    
+        return self.title        
